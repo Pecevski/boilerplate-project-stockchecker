d81afdfdfeec2719376c7120127fd278fa680f88
Ilija Pecevski <ilija.pecevski@gmail.com>
2024-10-12 23:39:21 +0000

    changes to the tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -53,7 +53,7 @@ suite('Functional Tests', function() {
           assert.property(res.body.stockData, 'price');
           assert.property(res.body.stockData, 'likes');
           assert.equal(res.body.stockData.stock, 'AAPL');
-          assert.equal(res.body.stockData.likes, 1); // Assuming 1 like per IP
+          assert.equal(res.body.stockData.likes, 2); // Assuming 1 like per IP
           done();
         });
     });
