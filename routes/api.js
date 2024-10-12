6d474b2368aefb7e4c960446d8188906356288b6
Ilija Pecevski <ilija.pecevski@gmail.com>
2024-10-12 23:11:15 +0000

    Refactoring

--- routes/api.js
+++ routes/api.js
@@ -10,29 +10,30 @@ module.exports = function (app) {
   };
 
   app.route('/api/stock-prices')
-    .get( async function (req, res){
-      const {stock, like} = req.query;
+    .get(async function (req, res) {
+      const { stock, like } = req.query;
       const ip = req.ip;
       const hashedIP = await hashIP(ip);
 
+      // Mock database interaction
       const stockData = (stock, likes = 0) => ({
         stock,
-        price: Math.floor(Math.random() * 1000),
+        price: Math.floor(Math.random() * 1000), // Mock price
         likes
       });
 
       if (Array.isArray(stock)) {
         const stock1 = stockData(stock[0], like ? 1 : 0);
         const stock2 = stockData(stock[1], like ? 1 : 0);
-        const rel_likes = stock1.likes - stock2.likes;
+        const rel_likes1 = stock1.likes - stock2.likes;
+        const rel_likes2 = stock2.likes - stock1.likes;
         res.json([
-          { stock: stock1.stock, price: stock1.price, rel_likes },
-          { stock: stock2.stock, price: stock2.price, rel_likes: -rel_likes }
+          { stock: stock1.stock, price: stock1.price, rel_likes: rel_likes1 },
+          { stock: stock2.stock, price: stock2.price, rel_likes: rel_likes2 }
         ]);
       } else {
         const response = stockData(stock, like ? 1 : 0);
         res.json({ stockData: response });
       }
     });
-    
 };
